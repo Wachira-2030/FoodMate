74b2cdb8e1fa97ce34dfe16160930f1fcc060148
Lucille Wanjiku <42667708+Wachira-2030@users.noreply.github.com>
2023-04-12 22:16:30 +0300

    Streamlit deployment

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import pandas as pd
-from scipy.spatial.distance import cosine, euclidean, hamming
-from flask_table import Table, Col
+import numpy as np
+import pickle
+from recipe_recommender import RecipeRecommender
 
-class Results(Table):
-    id = Col('id', show=False)
-    title = Col('name')
+# Unpickling the instance from the file
+#with open('recommender.pickle', 'rb') as f:
+ #   recommender = pickle.load(f)
 
-app = Flask(__name__)
+#pickle_in = open("recommender.pkl")
+#recommender = pickle.load(pickle_in)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+df = pd.read_csv("cleaned_data.csv")
+
+recommender = RecipeRecommender(df)
+
+def app():
+    st.title("Recipe Recommender")
+    
+    target_calories = st.slider("Select your target calories:", 0, 1000, 500, 10)
+    
+    num_recommendations = st.slider("Select the number of recommendations:", 1, 10, 5, 1)
+    
+    if st.button("Recommend"):
+        recommendations = recommender.recommend(target_calories, num_recommendations)
+        for i, recommendation in enumerate(recommendations):
+            st.write(f"Recipe {i+1}")
+            st.write(f"Name: {recommendation[0]}")
+            st.write(f"Ingredients: {recommendation[1]}")
+            st.write(f"Steps: {recommendation[2]}")
+
+if __name__ == "__main__":
+    app()
